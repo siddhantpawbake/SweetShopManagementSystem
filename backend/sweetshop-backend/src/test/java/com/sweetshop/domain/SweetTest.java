2c7c0e630721ae34584f4cb176d9cd44fab61f1f
siddhantpawbake <sidpawbake15@gmail.com>
2025-12-14 18:03:11 +0530

    test: add validation tests for negative price and quantity

--- backend/sweetshop-backend/src/test/java/com/sweetshop/domain/SweetTest.java
+++ backend/sweetshop-backend/src/test/java/com/sweetshop/domain/SweetTest.java
@@ -21,4 +21,25 @@ public class SweetTest {
         assertEquals(500.0, sweet.getPrice(), 0.001);
         assertEquals(10, sweet.getQuantity());
     }
+    @Test(expected = IllegalArgumentException.class)
+public void shouldNotAllowNegativePrice() {
+    new Sweet(
+            "2",
+            "Gulab Jamun",
+            "Indian",
+            -100.0,
+            5
+    );
+}
+@Test(expected = IllegalArgumentException.class)
+public void shouldNotAllowNegativeQuantity() {
+    new Sweet(
+            "3",
+            "Rasgulla",
+            "Indian",
+            200.0,
+            -3
+    );
+}
+
 }
